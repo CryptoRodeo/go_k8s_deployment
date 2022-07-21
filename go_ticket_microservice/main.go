6c13f3de0fab44561024a8c45f631925e1660804
Kilgore Trout <b.r.tech95@gmail.com>
2022-07-21 07:28:41 -0400

    move dummy data out onto its own file.

--- go_ticket_microservice/main.go
+++ go_ticket_microservice/main.go
@@ -6,23 +6,10 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/CryptoRodeo/go_k8s_deployment/tickets/data"
 	"github.com/gin-gonic/gin"
 )
 
-type Ticket struct {
-	UserId      int    `json:"user_id"`
-	TicketID    int    `json:"ticket_id"`
-	Description string `json:"description"`
-	IsUrgent    bool   `json:"is_urgent"`
-}
-
-// For now we'll just throw this dummy data out.
-var tickets = []Ticket{
-	{1, 1800, "The netflix wont work!", true},
-	{2, 1500, "Need new mouse", false},
-	{3, 2000, "Donut ordering microservice is down!", true},
-	{3, 2001, "Pizza ordering microservice is down too!", true},
-}
 var PORT string = fmt.Sprintf(":%s", getEnv("PORT", "8081"))
 var APP_NAME string = getEnv("APP_NAME", "go-ticket")
 
@@ -37,7 +24,7 @@ func main() {
 
 func getTickets(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"data": tickets,
+		"data": data.Tickets,
 	})
 }
 
@@ -67,9 +54,9 @@ func getUserTickets(c *gin.Context) {
 	})
 }
 
-func findUserTickets(user_id int) ([]Ticket, bool) {
-	temp := []Ticket{}
-	for _, ticket := range tickets {
+func findUserTickets(user_id int) ([]data.Ticket, bool) {
+	temp := []data.Ticket{}
+	for _, ticket := range data.Tickets {
 		if ticket.UserId == user_id {
 			temp = append(temp, ticket)
 		}
