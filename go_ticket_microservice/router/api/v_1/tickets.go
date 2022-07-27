5c25872fc972af7572cfcdf87d722f1f1de5e216
Kilgore Trout <b.r.tech95@gmail.com>
2022-07-27 07:42:34 -0400

    search for ticket by id in "controller"

--- go_ticket_microservice/router/api/v_1/tickets.go
+++ go_ticket_microservice/router/api/v_1/tickets.go
@@ -2,9 +2,11 @@ package v_1
 
 import (
 	"encoding/json"
+	"fmt"
 	"go_ticket_microservice/service/ticket_service"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +20,31 @@ func GetAllTickets(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"data": tickets,
+	})
+}
+
+func GetTicket(c *gin.Context) {
+	_id := c.Param("id")
+
+	id, err := strconv.Atoi(_id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	if !ticket_service.ExistsByID(id) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("Ticket at id %d does not exist.", id),
+		})
+		return
+	}
+
+	ticket := ticket_service.GetTicketByID(id)
+	c.JSON(http.StatusOK, gin.H{
+		"data": ticket,
 	})
 }
 
