0891665c20a4b14d348f63a898281b0078421143
Kilgore Trout <b.r.tech95@gmail.com>
2022-07-27 07:42:47 -0400

    search for tickets by ID in service

--- go_ticket_microservice/service/ticket_service/ticket_service.go
+++ go_ticket_microservice/service/ticket_service/ticket_service.go
@@ -8,6 +8,10 @@ func GetAllTickets() []models.Ticket {
 	return models.GetAllTickets()
 }
 
+func GetTicketByID(id int) *models.Ticket {
+	return models.GetTicket(id)
+}
+
 func SearchTickets(ids []int) []*models.Ticket {
 	return models.GetTicketsByID(ids)
 }
