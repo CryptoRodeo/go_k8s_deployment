d20cc569857363748b0c4061c5eea225f043f1e9
Kilgore Trout <b.r.tech95@gmail.com>
2022-07-28 06:43:48 -0400

    set runmode and other vars in server settings

--- go_ticket_microservice/main.go
+++ go_ticket_microservice/main.go
@@ -5,6 +5,8 @@ import (
 	"go_ticket_microservice/utils/settings"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -12,14 +14,19 @@ func init() {
 }
 
 func main() {
-	routersInit := router.InitRouter()
+	gin.SetMode(settings.ServerSettings.RunMode)
+	routersInit := router.Init()
 
 	port := settings.ServerSettings.Port
 	appName := settings.ServerSettings.AppName
+	readTimeout := settings.ServerSettings.ReadTimeout
+	writeTimeout := settings.ServerSettings.WriteTimeout
 
 	server := &http.Server{
-		Addr:    port,
-		Handler: routersInit,
+		Addr:         port,
+		Handler:      routersInit,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
 	}
 
 	log.Printf("[info] starting HTTP server for %s, listening on port %s", appName, port)
--- go_ticket_microservice/utils/settings/settings.go
+++ go_ticket_microservice/utils/settings/settings.go
@@ -3,11 +3,15 @@ package settings
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 type AppSettings struct {
-	Port    string
-	AppName string
+	Port         string
+	AppName      string
+	RunMode      string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
 }
 
 var ServerSettings = &AppSettings{}
@@ -15,6 +19,9 @@ var ServerSettings = &AppSettings{}
 func Setup() {
 	ServerSettings.Port = fmt.Sprintf(":%s", getEnv("PORT", "8081"))
 	ServerSettings.AppName = getEnv("APP_NAME", "go-ticket")
+	ServerSettings.RunMode = getEnv("RUN_MODE", "debug") // or release
+	ServerSettings.ReadTimeout = (60 * time.Second)
+	ServerSettings.WriteTimeout = (60 * time.Second)
 }
 
 func getEnv(key, fallback string) string {
