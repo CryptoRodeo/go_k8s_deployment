42b55eb1c89490841c60d02bfd41caeeda962192
Kilgore Trout <b.r.tech95@gmail.com>
2022-07-28 20:04:44 -0400

    disable users search for now.

--- go_user_microservice/router/router.go
+++ go_user_microservice/router/router.go
@@ -5,6 +5,7 @@ import (
 	"go_user_microservice/utils/settings"
 
 	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,9 +18,9 @@ func Init() *gin.Engine {
 	apiv1 := r.Group("/api/v1")
 	{
 		apiv1.GET("/users", v_1.GetAllUsers)
-		apiv1.GET("/user/:id", v_1.GetUserByID)
-		apiv1.GET("/user/search", v_1.GetUsersByID)
-		apiv1.GET("/user/:id/tickets", v_1.GetUsersTickets)
+		apiv1.GET("/users/:id", v_1.GetUserByID)
+		// apiv1.GET("/user/search", v_1.GetUsersByID)
+		apiv1.GET("/users/:id/tickets", v_1.GetUsersTickets)
 	}
 
 	return r
