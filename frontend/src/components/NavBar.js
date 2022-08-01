fe5809bc970e0c1eec4d91514eda402dd355c6bd
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-01 06:54:06 -0400

    update nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,37 +4,33 @@ export default function NavBar(props) {
 	return (
 		<nav className="navbar is-black" role="navigation" aria-label="main navigation">
 			<div className="navbar-brand">
-				<a className="navbar-item" href="https://bulma.io">
+				<a className="navbar-item" href="#">
 					<h1 className="title has-text-primary">TicketMaster</h1>
 				</a>
 			</div>
 
-			<a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-			</a>
 
-			<div className="navbar-menu has-text-white-ter">
+			<div id="mainNavbar" className="navbar-menu has-text-white-ter">
 				<div className="navbar-start">
 					<a className="navbar-item">
 						Tickets
 					</a>
 				</div>
-			</div>
 
-			<div className="navbar-end">
-				<div className="navbar-item">
-					<div className="buttons">
-						<a className="button is-primary">
-							<strong>Sign Up</strong>
-						</a>
-						<a className="button is-light">
-							Log in
-						</a>
+				<div className="navbar-end">
+					<div className="navbar-item">
+						<div className="buttons">
+							<a className="button is-primary">
+								<strong>Sign Up</strong>
+							</a>
+							<a className="button is-light">
+								Log in
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
+
 		</nav>
 	)
 }
