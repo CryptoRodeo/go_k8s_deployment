7328419c1120b86f7eaefc1c2313919361aa2626
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-01 06:55:35 -0400

    set up cors settings for ticket microservice

--- go_ticket_microservice/utils/settings/settings.go
+++ go_ticket_microservice/utils/settings/settings.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/gin-contrib/cors"
 )
 
 type AppSettings struct {
@@ -16,12 +18,20 @@ type AppSettings struct {
 
 var ServerSettings = &AppSettings{}
 
+var Cors = cors.Config{}
+
 func Setup() {
 	ServerSettings.Port = fmt.Sprintf(":%s", getEnv("PORT", "8081"))
 	ServerSettings.AppName = getEnv("APP_NAME", "go-ticket")
 	ServerSettings.RunMode = getEnv("RUN_MODE", "debug") // or release
 	ServerSettings.ReadTimeout = (60 * time.Second)
 	ServerSettings.WriteTimeout = (60 * time.Second)
+	// CORS
+	frontend := getEnv("FRONT_END_ENDPINT", "http://localhost:3000")
+	Cors.AllowOrigins = []string{frontend}
+	Cors.AllowMethods = []string{"GET"}
+	Cors.AllowHeaders = []string{"Origin", "Content-Type"}
+	Cors.AllowCredentials = true
 }
 
 func getEnv(key, fallback string) string {
