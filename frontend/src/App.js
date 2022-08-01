15a40d7dd70cf308fb7200c4498152db44f4d47d
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-01 06:53:58 -0400

    swap user list for ticket list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
-import UserList from './components/UserList';
+import TicketList from './components/TicketList';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App container is-fullhd ">
+    <div className="App container is-fullhd has-background-light">
     <NavBar/>
-    <UserList />
+    <TicketList/>
     </div>
   );
 }
