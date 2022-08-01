28e9339c08aed83441a790cff6e0671e37a59f49
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-01 06:55:02 -0400

    update user service file

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,12 +1,12 @@
-const ENDPOINT = "http://localhost:8080/api/v1/users"
 
 /**
  * 
- * @param {*} stateCb state callback
- * @param {*} errorCb  error callback
+ * @param {*} stateCb React hook state callback
+ * @param {*} errorCb  React hook error callback
  */
 async function getUsers(stateCb, errorCb) {
-	await fetch(ENDPOINT,
+	const users_endpoint = "http://localhost:8080/api/v1/users"
+	await fetch(users_endpoint,
 		{
 			mode: 'cors',
 			headers: {
@@ -17,6 +17,28 @@ async function getUsers(stateCb, errorCb) {
 		.then((res) => stateCb(res.data), (err) => errorCb(err)) 
 }
 
+/**
+ * 
+ * @param {*} user_id int
+ * @param {*} errorCb React hook error callback
+ */
+async function getUserTickets(user_id, errorCb) {
+	const user_tickets_endpoint = `http://localhost:8080/api/v1/users/${user_id}/tickets`
+	let tickets;
+
+	await fetch(user_tickets_endpoint, {
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		}
+	})
+	.then(res => res.json())
+	.then((res) => tickets = res)
+
+	return tickets;
+}
+
 export default {
 	getUsers,
+	getUserTickets,
 }
