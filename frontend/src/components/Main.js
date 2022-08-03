d0134239c5a4db05b2c0fa3580d3dd3d0ecab8c3
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-03 08:46:08 -0400

    pass props to menu, toggle its display

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,13 +1,18 @@
 import './Main.scss'
 import React, { useState, useEffect } from 'react';
+import NavBar from './NavBar';
 import TicketList from './TicketList';
 import Menu from './Menu';
 
 
-export default function Main() {
+export default function Main(props) {
+
+	const menu_styles = {
+		"display": (props.showMenu ? 'block' : 'none'),
+	}
 	return (
 		<div className="columns">
-			<div className="column menu-column">
+			<div className="column menu-column" style={menu_styles}>
         <Menu />
 			</div>
 			<div className="column data-column">
