28f6cb6b487c9b3277763d80603da5121c1d1ab2
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-05 15:12:54 -0400

    update UserService

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -20,12 +20,11 @@ async function getUsers(stateCb, errorCb) {
 /**
  *
  * @param {*} user_id int
+ * @param {*} stateCb React hook state callback
  * @param {*} errorCb React hook error callback
  */
-async function getUserTickets(user_id, errorCb) {
+async function getUserTickets(user_id, stateCb, errorCb) {
 	const user_tickets_endpoint = `http://localhost:8080/api/v1/users/${user_id}/tickets`
-	let tickets;
-
 	await fetch(user_tickets_endpoint, {
 		mode: 'cors',
 		headers: {
@@ -33,12 +32,29 @@ async function getUserTickets(user_id, errorCb) {
 		}
 	})
 	.then(res => res.json())
-	.then((res) => tickets = res)
+	.then((res) => stateCb(res.data), (err) => errorCb(err))
+}
 
-	return tickets;
+/**
+ *
+ * @param {*} user_id int
+ * @param {*} stateCb React hook state callback
+ * @param {*} errorCb React hook error callback
+ */
+async function getUserByID(user_id, stateCb, errorCb) {
+	const user_tickets_endpoint = `http://localhost:8080/api/v1/user/${user_id}`
+	await fetch(user_tickets_endpoint, {
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		}
+	})
+	.then(res => res.json())
+	.then((res) => stateCb(res.data), (err) => errorCb(err))
 }
 
 export default {
 	getUsers,
+	getUserByID,
 	getUserTickets,
 }
