702c7f13e74808e3569cddf10100ced153b36845
Kilgore Trout <b.r.tech95@gmail.com>
2022-08-05 15:10:11 -0400

    add route for user ticket list

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,12 +4,13 @@ import TicketList from './TicketList';
 import UserList from './UserList';
 import Menu from './Menu';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import UserTicketList from './UserTicketList';
 
 export default function Main(props) {
-
 	const menu_styles = {
 		"display": (props.showMenu ? 'block' : 'none'),
 	}
+
 	return (
 		<div className="columns">
 			<div className="column menu-column" style={menu_styles}>
@@ -20,6 +21,7 @@ export default function Main(props) {
 					<Routes>
 						<Route path="/" element={<TicketList />} />
 						<Route path="/users" element={<UserList/>} />
+						<Route path="/user/:id/tickets" element={ <UserTicketList /> } />
 					</Routes>
 				</BrowserRouter>
 			</div>
